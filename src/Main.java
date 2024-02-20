d358fd9caeaa9a587fce0d33e50970cfac578751
Calyen93 <genco.claudio3@gmail.com>
2024-02-20 10:45:29 +0100

    first commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+/*
+-Scrivere un programma che contenga una classe chiamata Forma ed un metodo
+chiamato calcolaArea() che stampi l'area della forma.
+
+-Crea poi una sottoclasse chiamata Rettangolo che fa override del metodo calcolaArea()
+per calcolare l'area del rettangolo.*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        Shape shape = new Shape();
+
+        shape.calculateArea();
+
+        Rectangle rectangle = new Rectangle(31, 42);
+
+        rectangle.calculateArea();
+    }
+}
